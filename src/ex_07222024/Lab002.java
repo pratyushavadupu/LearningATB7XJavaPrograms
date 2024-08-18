58b32e05534e195baa6ed926253c7f2cc18ce72f
Pratyushav <pratyushaa.vadupu92@gmail.com>
2024-08-18 18:38:24 +0530

    test commit

--- src/ex_07222024/Lab002.java
+++ src/ex_07222024/Lab002.java
@@ -2,6 +2,6 @@ package ex_07222024;
 
 public class Lab002 {
     public static void main(String[] arg) {
-
+        System.out.println("Hello World Pratyusha!");
     }
 }
---
+++ src/ex_07222024/StringOps.java
@@ -0,0 +1,25 @@
+package ex_07222024;
+
+public class StringOps {
+
+        public static void main(String[] args) {
+            int a = 10;
+            int b = 34;
+            System.out.println(a+b);
+
+            String s1 = "Pratyu";
+            String s2 = "Vadupu";
+            System.out.println(s1+s2);
+
+            //If you print output starting with string
+            String s3 = "Naksh";
+            int x = 99;
+            int y = 100;
+            System.out.println(x+y);
+            System.out.println(x+s3);
+            System.out.println(s3+y);
+            System.out.println(x+s3+y);
+            System.out.println(x+y+s3);
+        }
+    }
+
